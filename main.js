da8be7da364048d6ff118271473883d9f6999753
Potatoritos <potatoritos@gmail.com>
2022-05-28 15:10:07 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let a = {};
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Graph {
+    constructor() {
+        this.adjList = [];
+    }
+
+    addEdge(u, v) {
+        
+    }
+}
+
+
+
+
+
+
+console.log(a);
+
+
