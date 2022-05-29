e4c661894ce9827c70ac2f599d6f6fc6b5b514b6
br0mabs <steven.lol.hax@gmail.com>
2022-05-29 00:31:05 -0400

    add topo sort

--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,45 @@
+class Queue {
+    constructor() {
+      this.elements = {};
+      this.head = 0;
+      this.tail = 0;
+    }
+    enqueue(element) {
+      this.elements[this.tail] = element;
+      this.tail++;
+    }
+    dequeue() {
+      const item = this.elements[this.head];
+      delete this.elements[this.head];
+      this.head++;
+      return item;
+    }
+    peek() {
+      return this.elements[this.head];
+    }
+    get length() {
+      return this.tail - this.head;
+    }
+    get isEmpty() {
+      return this.length === 0;
+    }
+  }
+  
+  /*let q = new Queue();
+  for (let i = 1; i <= 7; i++) {
+    q.enqueue(i);
+  }
+  // get the current item at the front of the queue
+  console.log(q.peek()); // 1
+  
+  // get the current length of queue
+  console.log(q.length); // 7
+  
+  // dequeue all elements
+  while (!q.isEmpty) {
+    console.log(q.dequeue());
+  }
+}*/
 class Graph {
     constructor() {
         this.parents = new AdjList();
@@ -143,6 +185,34 @@ class Graph {
         }
 
         (elemDrag || elem).onmousedown = onMouseDown;
+    }
+    arrangeAsTree() {
+        let len = Object.keys(this.nodes).length;
+        let arr = Array.from(Array(len));
+        let num = Array.from(Array(len));
+        let lvl = 1;
+        let q = new Queue();
+        // for each node we should record how many parents they have
+        for (let i = 0; i < len; ++i) {
+            num[i] = this.parent.get(i).length;
+            if (num[i] === 0) {
+                arr[i] = 1;
+                q.enqueue(i);
+            }
+        }
+        // go through and assign levels to children, removing edges in the process
+        while (!q.isEmpty()) {
+            let u = q.dequeue();
+            this.child.get(u).forEach(v => {
+                arr[v] = arr[u] + 1;
+                --num[v];
+                if (num[v] == 0) {
+                    q.enqueue(v);
+                }
+            });
+        }
+        // now we know the levels, so we should sort the nodes based on the levels
+
     }
 }
 
