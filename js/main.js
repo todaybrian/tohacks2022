d001b2bab84f560d9df417b99fd533d41f51fb62
todaybrian <todaybryan@gmail.com>
2022-05-29 11:28:50 -0400

    Fix default node positioning

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ g = new Graph();
 
 const purple = '#d5bdfc';
 
-g.addNode(0, 400, 100, false, 'Node 0', purple);
-g.addNode(1, 400, 150, false, 'Node 1', purple);
-g.addNode(2, 300, 200, false, 'Node 2', purple);
-g.addNode(3, 500, 200, false, 'Node 3', purple);
-g.addNode(4, 500, 250, false, 'Node 4', purple);
-g.addNode(5, 500, 400, false, 'Node 5', purple);
-g.addNode(6, 300, 400, false, 'Node 6 sdfkj sdlkfk la asjflj asljfjkl;akl;jaskl;fjlasdjflk', purple);
+g.addNode(0, 400, 130, false, 'Node 0', purple);
+g.addNode(1, 400, 180, false, 'Node 1', purple);
+g.addNode(2, 300, 230, false, 'Node 2', purple);
+g.addNode(3, 500, 230, false, 'Node 3', purple);
+g.addNode(4, 500, 280, false, 'Node 4', purple);
+g.addNode(5, 500, 430, false, 'Node 5', purple);
+g.addNode(6, 300, 430, false, 'Node 6 sdfkj sdlkfk la asjflj asljfjkl;akl;jaskl;fjlasdjflk', purple);
 
 g.addEdge(0, 1);
 g.addEdge(1, 2);
