877060ae78385efa69e3201c3b5374d78daac5b7
psun256 <paulsun700@gmail.com>
2022-05-28 22:13:41 -0400

    Message (Ctrl+Enter to commit on 'task_status')

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 g = new Graph();
 
-g.addNode(0);
-g.addNode(1);
-g.addNode(2);
-g.addNode(3, 500, 500);
-g.addNode(4, 400, 400);
-g.addNode(5, 400, 500);
-g.addNode(6, 200, 500);
+g.addNode(0, 400, 100);
+g.addNode(1, 400, 150);
+g.addNode(2, 300, 200);
+g.addNode(3, 500, 200);
+g.addNode(4, 500, 250);
+g.addNode(5, 500, 400);
+g.addNode(6, 300, 400);
 
 g.addEdge(0, 1);
 g.addEdge(1, 2);
